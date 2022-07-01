866f9cd4e6043d7036a178e774c07f95fa5c1d41
Matheus Santos <matheusOS2006@outlook.com>
2022-06-30 21:30:11 -0300

    30/06 - Sequência da API com Node.js

--- node/examples/read-file/index.js
+++ node/examples/read-file/index.js
@@ -1,20 +1,61 @@
-const http = require('http');
-const URL = require('url');
-const users = require('./users.json');
-
 // Disponibilizamos recursos para o usuário, que os requisita
 // Recursos são tudo que possuímos e mostramos para o Usuário 
 
 // Request são os atributos e métodos, é de onde vem a requisição
 // Response é a resposta que será dada pelo servidor a request
-http.createServer((req, res) => {
-  const {name, lastName, age} = URL.parse(req.url, true).query;
+
+const http = require('http');
+const URL = require('url');
+const fs = require('fs');
+const path = require('path');
+let users = require('./users.json');
+
+const getUsers = (request, response) => {
+  const {name, lastName, age, remove} = URL.parse(request.url, true).query;
+
+  let message = '';
 
   if (name || lastName || age) { 
-    console.log('query', name, lastName, age)
+    const user = {
+      name, lastName, age
+    }
+
+    if (remove) {
+      message = 'Registro removido com suceso';
+      const found = users.filter(user => user.name == name)
+      users = users.filter(user => user.name != name)
+
+      if (!found.length) {
+        response.end('Usuário não encontrado');
+      }
+    }else{
+      message = 'Registro salvo com sucesso';
+      users.push(user);
+    }
+
+    // 1° parâmetro : caminho absoluto do arquivo, 2° arquivo que será enviado, 3° callback function
+    fs.writeFile(
+      path.join(__dirname, 'users.json'),
+      JSON.stringify(users, null, 2),
+      (error) => {
+        if (error) return; 
+
+        console.log('Salvou o registro com sucesso')
+        response.end(JSON.stringify({
+          status: message,
+          data: user
+        }))
+      }
+    )
+  }else{
+    // Resposta para o cliente
+    response.end(JSON.stringify(users));
   }
-  // Resposta para o cliente
-  res.end(JSON.stringify(users));
-}).listen(6924, () => {
-  console.log('API is running on port 6924');
+}
+
+const server = http.createServer(getUsers);
+
+server.listen(6924, (request, response) => {
+  console.log('API listening on http://localhost:6924');
 })
+
